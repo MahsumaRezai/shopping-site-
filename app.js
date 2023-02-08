cdb5fb28dfa8cb7a162a14d272bf028ba0c328c8
Your Name <you@example.com>
2023-02-08 10:50:27 -0800

    start work on js code

---
+++ app.js
@@ -0,0 +1,35 @@
+function myFunction(element, color) {
+    element.style.color = color;
+}
+
+function button() {
+    prompt("Do you wont boy now");
+    if (x = "ok") {
+        alert("You can boy now")
+    }
+    else if (x = "Cancel") {
+        alert("You can not boy now")
+
+    }
+
+}
+// head input 
+function input_haed() {
+    document.getElementById("input").value = "Can you search this here"
+}
+function country() {
+    document.getElementById("link").innerHTML = "USA"
+}
+
+// footer function 
+function changfooter_color(element, color) {
+    element.style.color = color
+
+}
+// footer email massage 
+function email() {
+    alert("Write your Emali Address")
+}
+
+
+
